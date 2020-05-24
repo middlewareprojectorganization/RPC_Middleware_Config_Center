d7973f28aefe61a20e6cf09a9f7dff598445d1e5
xiangxy26878 <xiangxy26878@hundsun.com>
2020-05-24 18:09:15 +0800

    first commit

---
+++ rpc_middleware_config_center_plat/src/main/java/com/xxy/rpc/rpc_middleware_config_center_plat/RpcMiddlewareConfigCenterPlatApplication.java
@@ -0,0 +1,13 @@
+package com.xxy.rpc.rpc_middleware_config_center_plat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcMiddlewareConfigCenterPlatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcMiddlewareConfigCenterPlatApplication.class, args);
+    }
+
+}
---
+++ rpc_middleware_config_center_plat/src/test/java/com/xxy/rpc/rpc_middleware_config_center_plat/RpcMiddlewareConfigCenterPlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxy.rpc.rpc_middleware_config_center_plat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcMiddlewareConfigCenterPlatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
