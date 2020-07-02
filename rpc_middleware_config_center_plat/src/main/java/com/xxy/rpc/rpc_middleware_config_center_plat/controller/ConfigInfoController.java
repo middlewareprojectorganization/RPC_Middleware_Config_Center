1b19523e5603a313c5dbe3395cbb3e323f115d5d
xiangxy26878 <xiangxy26878@hundsun.com>
2020-07-02 22:51:36 +0800

    Interface request specification

--- rpc_middleware_config_center_plat/src/main/java/com/xxy/rpc/rpc_middleware_config_center_plat/controller/ConfigInfoController.java
+++ rpc_middleware_config_center_plat/src/main/java/com/xxy/rpc/rpc_middleware_config_center_plat/controller/ConfigInfoController.java
@@ -1,6 +1,9 @@
 package com.xxy.rpc.rpc_middleware_config_center_plat.controller;
 
 
+import com.xxy.rpc.rpc_middleware_config_center_plat.dto.ConfigInfoDTO;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author vicente
  * @since 2020-05-29
  */
+@Api(tags = "配置管理相关接口")
 @RestController
-@RequestMapping("/config-info")
+@RequestMapping("/config/info")
 public class ConfigInfoController {
+    /**
+     * 拉取默认配置
+     * @return
+     */
+    @GetMapping("/default/get")
+    public ConfigInfoDTO getConfig(){
+        return new ConfigInfoDTO();
+    }
+
+
 
 }
---
+++ rpc_middleware_config_center_plat/src/main/java/com/xxy/rpc/rpc_middleware_config_center_plat/dto/ConfigInfoDTO.java
@@ -0,0 +1,42 @@
+package com.xxy.rpc.rpc_middleware_config_center_plat.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: XXY
+ * @Date: 2020/6/13 21:44
+ */
+@Data
+@ApiModel
+public class ConfigInfoDTO {
+
+    /**
+     * 配置id
+     */
+    @ApiModelProperty(value = "配置id")
+    private String configId;
+
+    /**
+     * 配置内容
+     */
+    @ApiModelProperty(value = "配置内容")
+    private String configContent;
+
+    /**
+     * 配置名称
+     */
+    @ApiModelProperty(value = "配置名称")
+    private String configName;
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime gmtCreate;
+    @ApiModelProperty("修改时间")
+    private LocalDateTime gmtModified;
+
+
+}
